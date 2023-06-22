a394eb0cdf2d3477e961112aaceaa8a273c8323b
Sulaxan <sulaxan33@yahoo.ca>
2023-06-22 00:12:41 -0400

    fix integration tests

--- lol-item-analyzer/tests/integration.rs
+++ lol-item-analyzer/tests/integration.rs
@@ -1,16 +1,7 @@
 use std::{cell::RefCell, rc::Rc};
 
 use lol_item_analyzer::{
-    data::lol_api::LolApi,
-    item::{
-        gv::stat::StatGVTableGenerator,
-        transformer::{
-            id_assoc::IdAssociatorTransformer, item_value::ItemValueTransformer,
-            masterwork_assoc::MasterworkAssociatorTransformer,
-            masterwork_ident::MasterworkIdentifierTransformer,
-            masterwork_item_value::MasterworkItemValueTransformer, TransformHandler,
-        },
-    },
+    data::lol_api::LolApi, item::gv::stat::StatGVTableGenerator, ItemAnalyzer, StatAnalyzer,
 };
 
 #[tokio::test]
@@ -19,9 +10,12 @@ async fn generate_gv_table() {
     let items = Rc::new(RefCell::new(
         LolApi::get_items(&latest_version).await.unwrap(),
     ));
-    let stats = LolApi::get_stat_ids(&latest_version).await.unwrap();
+    let raw_stats = LolApi::get_stat_ids(&latest_version).await.unwrap();
 
-    let table_computer = StatGVTableGenerator::new(items.clone(), stats);
+    let handler = StatAnalyzer::new(raw_stats, StatAnalyzer::default_transformers());
+    let stats = Rc::new(RefCell::new(handler.transform_handler.transform_all()));
+
+    let table_computer = StatGVTableGenerator::new(items.clone(), stats.clone());
     let table = table_computer.generate();
 
     println!("{:#?}", table);
@@ -30,22 +24,17 @@ async fn generate_gv_table() {
 #[tokio::test]
 async fn everything_works() {
     let latest_version = LolApi::get_latest_version().await.unwrap();
-    let items = LolApi::get_items(&latest_version).await.unwrap();
-    let stats = LolApi::get_stat_ids(&latest_version).await.unwrap();
-    println!("Stats: {:#?}", stats);
+    let raw_items = LolApi::get_items(&latest_version).await.unwrap();
+    let raw_stats = LolApi::get_stat_ids(&latest_version).await.unwrap();
+    println!("Stats: {:#?}", raw_stats);
 
-    let transformer = TransformHandler::new(
-        items,
-        vec![
-            Rc::new(RefCell::new(IdAssociatorTransformer)),
-            Rc::new(RefCell::new(MasterworkIdentifierTransformer)),
-            Rc::new(RefCell::new(MasterworkAssociatorTransformer)),
-            Rc::new(RefCell::new(ItemValueTransformer::new(stats))),
-            Rc::new(RefCell::new(MasterworkItemValueTransformer)),
-        ],
+    let stat_analyzer = StatAnalyzer::new(raw_stats, StatAnalyzer::default_transformers());
+    let item_anaylzer = ItemAnalyzer::new(
+        raw_items,
+        ItemAnalyzer::default_transformers(&stat_analyzer),
     );
 
-    let new_items = transformer.transform_all();
+    let new_items = item_anaylzer.transform_handler.transform_all();
 
     let pickaxe_item = new_items.get("1037").unwrap();
     assert_eq!(pickaxe_item.is_masterwork, false);
